a2628454516ad246d15032f67c788e1ef6315391
mluukkai-cs <mluukkai@cs.helsinki.fi>
2016-11-23 21:13:58 +0200

    typofix

--- otmtestiappi/src/main/java/fi/helsinki/cs/otmtestiappi/Main.java
+++ otmtestiappi/src/main/java/fi/helsinki/cs/otmtestiappi/Main.java
@@ -4,6 +4,6 @@ package fi.helsinki.cs.otmtestiappi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("foobar");
-	System.out,println("uusi toiminto");
+	System.out.println("uusi toiminto");
     }
 }
