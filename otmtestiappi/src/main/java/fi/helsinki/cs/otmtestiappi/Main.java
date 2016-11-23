9a49cd301c6505eaeb274757b0874a6c58548965
Matti Luukkainen <mluukkai@iki.fi>
2016-11-23 21:12:58 +0200

    muutos

--- otmtestiappi/src/main/java/fi/helsinki/cs/otmtestiappi/Main.java
+++ otmtestiappi/src/main/java/fi/helsinki/cs/otmtestiappi/Main.java
@@ -4,5 +4,6 @@ package fi.helsinki.cs.otmtestiappi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("foobar");
+	System.out,println("uusi toiminto");
     }
 }
